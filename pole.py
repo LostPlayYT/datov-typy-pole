8621018342ad1dd22734b85145f5dcc4970a30e3
LostPlayYT <bluefireyt@seznam.cz>
2024-03-19 13:34:59 +0100

    pole.py

---
+++ pole.py
@@ -0,0 +1,5 @@
+            #0           #1         #2         #3            #4            #5
+arrays = ["Ferrari", "Maserati", "Pagani", "Alfa Romeo", "Lamborghini", "Bugatti"]
+
+for i in arrays:
+    print (i)
